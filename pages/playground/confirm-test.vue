af89ca2cc8d15dfcb3fff889361d277a6fa13805
dong o kim <otwmoo@gmail.com>
2019-03-04 23:28:44 +0900

    초기 인터페이스 코드

--- pages/playground/confirm-test.vue
+++ pages/playground/confirm-test.vue
@@ -1,14 +1,87 @@
 <template>
   <section class="container">
-    <confirm />
+    <confirm-dialog
+      :open="confirm"
+      @dismiss="No(close)"
+      @confirm="Yes(open)"
+    >
+      <div>
+        밥은 먹었니?
+      </div>
+    </confirm-dialog>
+    <button @click="confTest">ok</button>
   </section>
 </template>
 
 <script>
-  import Confirm from '../../components/Confirm'
+  import ConfirmDialog from '../../components/ConfirmDialog'
+
+  let confirmResolve;
+  const Confirm = ({ open }) => new Promise((resolve) => {
+    open()
+    confirmResolve = resolve
+  })
+
+  const Yes = (open) => {
+    open()
+    if ( confirmResolve ) confirmResolve(true)
+  }
+  const No = (close) => {
+    close()
+    if ( confirmResolve ) confirmResolve(false)
+  }
+
   export default {
     components: {
-      Confirm
+      ConfirmDialog
+    },
+    data() {
+      return {
+        confirm: false,
+      };
+    },
+    methods: {
+      async confTest () {
+        // case1
+        if (!await Confirm({
+          message: 'ok?',
+          open: this.openConfirm,
+        })) {
+          console.log('not ok');
+          return;
+        }
+
+        console.log('ok');
+
+        // case2
+        if (!await Confirm({
+          component: 'ok?',
+          open: this.openConfirm,
+        })) {
+          console.log('ok')
+          return;
+        }
+
+        console.log('ok');
+
+        // case3
+        if (!await Confirm({
+          confirm: 'ok?',
+          open: this.openConfirm,
+        })) {
+          console.log('ok')
+          return;
+        }
+
+        console.log('ok');
+      },
+      openConfirm () {
+        this.confirm = true
+      },
+      closeConfirm () {
+        this.confirm = false
+      },
+      Yes, No,
     }
   }
 </script>
