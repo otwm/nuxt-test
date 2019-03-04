8bddc0e9cf1f203291caeeb781598119e6c314d5
dong o kim <otwmoo@gmail.com>
2019-03-04 22:54:39 +0900

    Initial commit

---
+++ components/Confirm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  .container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+</style>
+
---
+++ pages/playground/confirm-test.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <confirm />
+  </section>
+</template>
+
+<script>
+  import Confirm from '../../components/Confirm'
+  export default {
+    components: {
+      Confirm
+    }
+  }
+</script>
+
+<style>
+  .container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+</style>
+
